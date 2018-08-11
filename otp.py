1ac5cd01133183225d77565170be1a03da1246f0
elegnate <37270569+elegnate@users.noreply.github.com>
2018-08-11 16:24:34 +0900

    Create otp.py

---
+++ otp.py
@@ -0,0 +1,44 @@
+
+import struct, time
+import base64
+
+from Crypto.Hash      import SHA256, HMAC
+
+
+
+class OTP(object):
+
+    def __init__(self, secret, period = 30):
+        self.secret = secret
+        self.period = period
+
+
+    def Verify(self, code):
+        code = int(code)
+        ts = int(time.time())
+        for i in range(-self.period, self.period + 1):
+            counter = ts + i
+            if self.GenerateOtp(counter) == code:
+                return True
+        return False
+
+
+    def GenerateOtp(self, counter = None):
+        # https://tools.ietf.org/html/rfc4226
+        if counter == None:
+            counter = int(time.time())
+
+        msg = struct.pack('>Q', counter)
+        digest = HMAC.new(self.secret, msg, SHA256).digest()
+
+        pos = digest[19] & 15
+        base = struct.unpack('>I', digest[pos:pos + 4])[0] & 0x7fffffff
+        token = base % 100000000
+        return token
+
+
+    def _CompareDigest(self, a, b):
+        rv = 0
+        for x, y in zip(a, b):
+            rv |= x ^ y
+        return rv == 0
